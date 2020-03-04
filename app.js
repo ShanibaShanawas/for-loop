6fc4a917ce72ba1ebcf837f1599b7fdaf04fb462
Shaniba Shanawas <shaniba.shanawas@gmail.com>
2020-03-04 20:49:54 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// FOr loops
+for (let i = 0; i < 12; i++) {
+console.log('Nummer ' + i);
+}
+const mat = ["potatis","Morötter","Bananer"];
+for (let i = 0; i < mat.length; i++){
+    console.log('Mat produkten är: ' + mat[i]);
+}
